2e3c748195794b9f125860b3e625853d86317fac
galihlarasprakoso <galihlprakoso@gmail.com>
2019-01-22 11:38:01 +0700

    realeased

---
+++ buyboxpredictor/api/resources/predictor.py
@@ -0,0 +1,57 @@
+from flask import request
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required
+
+import json
+import numpy as np
+from buyboxpredictor.lib import Predictor
+
+class PredictorResource(Resource):
+
+    method_decorators = [jwt_required]
+    
+    def post(self):
+        predictor = Predictor()
+        inputs = request.json
+
+        data_inputs = []
+
+        try:
+            data_inputs.append(inputs["seller_positive_feedback_rating"])
+            data_inputs.append(inputs["feedback_count"])
+            data_inputs.append(inputs["is_fullfilled_by_amazon"])
+            data_inputs.append(inputs["price"])        
+            data_inputs.append(inputs["maximum_hours"])
+            data_inputs.append(inputs["is_featured_merchant"])
+            data_inputs.append(inputs["shipping_price"])        
+        except:
+            return {"error:":self.error_msg_bad_input}, 400
+
+        try:
+            result = predictor.predict(data_inputs)
+
+            msg = "Item is Impossible to be on the BuyBox."
+            if result == True:
+                msg = "Item is Possible to be on the BuyBox."
+
+            return {"result": result, "msg": msg}, 201
+
+        except:
+
+            return {"error:":self.error_msg_internal_server_error}, 400
+
+    error_msg_bad_input = """
+        Bad inputs. Make sure your input has these variables with the following order : 
+        - seller_positive_feedback_rating
+        - feedback_count
+        - is_fullfilled_by_amazon
+        - price
+        - maximum_hours
+        - is_featured_merchant
+        - shipping_price
+    """
+
+    error_msg_internal_server_error = """
+        Something wrong on the API Server. Contact API Developer.
+    """
+        
--- buyboxpredictor/api/views.py
+++ buyboxpredictor/api/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from buyboxpredictor.api.resources import UserResource, UserList
+from buyboxpredictor.api.resources import UserResource, UserList, PredictorResource
 
 
 blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
@@ -10,3 +10,5 @@ api = Api(blueprint)
 
 api.add_resource(UserResource, '/users/<int:user_id>')
 api.add_resource(UserList, '/users')
+
+api.add_resource(PredictorResource, '/predict')
